8cd195379070e967d98a59b79d51989a7385fd5e
alagoz <65690057+ademalagoz@users.noreply.github.com>
2024-01-16 23:45:33 +0000

    layout-loading added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,7 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}>Main Layout{children}</body>
+      <body className={inter.className}>
+
+      <Navbar/>
+      {children}
+      <Footer/></body>
     </html>
   )
 }
