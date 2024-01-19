58d21603b39567bb5102e09dd77c7fbee4085916
alagoz <65690057+ademalagoz@users.noreply.github.com>
2024-01-19 12:05:24 +0000

    internal and external image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "/public/hero.png";
 
 export default function Home() {
-  console.log('hi');
   return (
-  <div>Home</div>
-  )
+    <div>
+      <Image src={Hero} alt="Hero" />
+      <div>
+        {/* For external usage also update next.config.js */}
+        <Image
+          width={500}
+          height={500}
+          src="https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="External Image"
+        />
+      </div>
+      ;
+    </div>
+  );
 }
