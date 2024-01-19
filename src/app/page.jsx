d52c3e942f9079e02d2491237528792a3eeb9088
alagoz <65690057+ademalagoz@users.noreply.github.com>
2024-01-19 12:08:03 +0000

    removed external image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,16 +6,14 @@ export default function Home() {
   return (
     <div>
       <Image src={Hero} alt="Hero" />
-      <div>
-        {/* For external usage also update next.config.js */}
+      {/* <div>
         <Image
           width={500}
           height={500}
           src="https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="External Image"
         />
-      </div>
-      ;
+      </div> */}
     </div>
   );
 }
