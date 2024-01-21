e9562d0e29232c751f07e381f072482b75785f61
alagoz <65690057+ademalagoz@users.noreply.github.com>
2024-01-21 00:47:26 +0000

    portfolio category created

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Button from "../../../components/button/button";
+import Image from "next/image";
 
-const Category = () => {
+const Category = ({ params }) => {
+  console.log(params);
   return (
-    <div>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
 
-export default Category
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
