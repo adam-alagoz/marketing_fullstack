7eb1ab2ae70b48098e6639ca37cbd78c4a194857
alagoz <65690057+ademalagoz@users.noreply.github.com>
2024-01-22 22:23:06 +0000

    contact page and dark mode bugs fixed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-import Button from "../../components/button/button";
+import Button from "../../components/button/Button";
 
 const Contact = () => {
   return (
