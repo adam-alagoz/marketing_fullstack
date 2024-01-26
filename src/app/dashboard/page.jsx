c17ba3b4c71b50d0692ac6300e583af943b92543
alagoz <65690057+ademalagoz@users.noreply.github.com>
2024-01-26 23:58:09 +0000

    classic fetch

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,24 +2,24 @@
 import React, { useState, useEffect } from "react";
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        cache: "no-store",
-      });
-      if (!res.ok) {
-        setError(true);
-      }
-      const responseData = await res.json();
-      setData(responseData);
-      setLoading(false);
-    };
-    getData();
-  }, []);
-  console.log("data", data);
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-store",
+  //     });
+  //     if (!res.ok) {
+  //       setError(true);
+  //     }
+  //     const responseData = await res.json();
+  //     setData(responseData);
+  //     setLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+  // console.log("data", data);
   return <div>Dashboard</div>;
 };
 
