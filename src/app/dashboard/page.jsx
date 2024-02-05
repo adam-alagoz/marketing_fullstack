0bb88b8bf62e817f88c58f5310b5c337cfc4822b
alagoz <65690057+ademalagoz@users.noreply.github.com>
2024-02-05 21:59:29 +0000

    Revert "classic fetch"
    This reverts commit c17ba3b4c71b50d0692ac6300e583af943b92543.

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,20 +3,25 @@ import React from "react";
 import useSWR from "swr";
 
 const Dashboard = () => {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(
-    "https://jsonplaceholder.typicode.com/posts",
-    fetcher
-  );
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+      if (!res.ok) {
+        setError(true);
+      }
+      const responseData = await res.json();
+      setData(responseData);
+      setLoading(false);
+    };
+    getData();
+  }, []);
   console.log("data", data);
-  console.log("err", error);
-  return (
-    <div>
-      {data?.map((item) => (
-        <p key={item.id}>{item.title}</p>
-      ))}
-    </div>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
